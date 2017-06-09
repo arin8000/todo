9c90a84697a5c08ef4ac75e1c12b47664b2a2eb3
Arin H <arinhakoopian@gmailcom>
2017-06-08 23:54:43 -0700

    add some functionality

--- login.php
+++ login.php
@@ -33,6 +33,7 @@ endif;
     <title>Login Below</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <div class="header">
--- register.php
+++ register.php
@@ -34,6 +34,7 @@ endif;
     <title>Register Below</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <div class="header">
